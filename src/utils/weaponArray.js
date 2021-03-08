3ea1ac07e034631e7573bc18360e40d37992f446
CDM Cat <zagapyan@protonmail.com>
2021-03-07 22:58:29 -0600

    [weapon array shortening]

--- src/utils/weaponArray.js
+++ src/utils/weaponArray.js
@@ -1,6 +1,11 @@
 import glove from "../images/glove.png"
 import pan from "../images/pan.png"
-import slipper from "../images/slipper.png"
-import pad from "../images/pad.png"
+// import slipper from "../images/slipper.png"
+// import pad from "../images/pad.png"
 
-export default [glove, pan, slipper, pad]
+export default [
+  glove,
+  pan,
+  // slipper,
+  // pad
+]
